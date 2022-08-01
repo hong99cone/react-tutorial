adbb040850c73ac51b4148865e48e154e81bd28b
hong <03mm007mm@gmail.com>
2022-08-01 17:13:17 +0900

    react : tutorial ~Adding Time Travel

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,6 +15,9 @@ import './index.css';
 //     }
 //   }
 
+
+
+
 function Square(props) {
     return (
         <button className = 'square' onClick = {props.onClick}>
@@ -64,12 +67,13 @@ function Square(props) {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber : 0,
             xIsNext : true,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -81,15 +85,36 @@ function Square(props) {
             history : history.concat([{
                 squares : squares
             }]),
+            stepNumber : history.length,
             xIsNext : !this.state.xIsNext,
         });
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber : step,
+        xIsNext : (step % 2) === 0,
+      });
     }
 
     render() {
 
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+        
+        const moves = history.map((step,move) => {
+          const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+          return (
+            <li key={move}>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+          )
+        })
+        
+        
         let status;
         if (winner) {
             status = "Winner : " + winner;
@@ -109,7 +134,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );

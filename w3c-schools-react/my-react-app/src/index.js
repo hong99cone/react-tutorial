da9ad102ee2fef6fd8b8c4d03dc167f180ee1a66
hong <03mm007mm@gmail.com>
2022-08-30 13:47:19 +0900

    w3c-schools-tutorial ~React Events

--- w3c-schools-react/my-react-app/src/index.js
+++ w3c-schools-react/my-react-app/src/index.js
@@ -3,4 +3,39 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <App />);
+
+
+function Car () {
+    return (
+        <h2>Hello I am Jeep ; </h2>
+    )
+}
+
+function Drum(props) {
+    return (
+        <div>
+            <p>Hey I am reall drummer {props.name}!! </p>
+            <p>I have a {props.brand} drum!</p>
+        </div>
+
+    )
+}
+
+
+function Band() {
+    function playMusic() {
+        alert("Jesus! It's not a music");
+    }
+    return(
+        <div>
+            <Drum 
+            name = 'J'
+            brand = 'Yamaha'
+            />
+            <button onClick = {playMusic}>play the song</button>
+        </div>
+    )
+}
+
+root.render(<Band />);
+
